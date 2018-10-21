7a06e90be72dfd1f665270c8a17ee633f7afe1d3
zerju <jure.zerak@gmail.com>
2018-10-21 17:38:48 +0200

    dont rerender modal text/data if modal not shown

--- burger-builder/src/components/UI/Modal/Modal.js
+++ burger-builder/src/components/UI/Modal/Modal.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import cssClasses from './Modal.module.css';
 import Backdrop from './../Backdrop/Backdrop';
 
-const modal = (props) => {
-  return (
-    <>
-      <Backdrop show={props.show} clicked={props.modalClosed} />
-      <div className={cssClasses.Modal}
-        style={{
-          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.show ? '1' : '0'
-        }}>
-        {props.children}
-      </div>
-    </>
-  );
-};
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+  render() {
+    return (
+      <>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div className={cssClasses.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+      </>
+    );
+  }
+}
 
-export default modal;
+export default Modal;
