e01933572f04d227f2e10d1ca8b5213e42ce5bec
zerju <jure.zerak@gmail.com>
2018-10-22 14:17:24 +0200

    orders page

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,8 +6,8 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = () => {
   return (
     <ul className={cssClasses.NavigationItems}>
-      <NavigationItem link="/" active>Burger Builder</NavigationItem>
-      <NavigationItem link="/">Checkout</NavigationItem>
+      <NavigationItem link="/">Burger Builder</NavigationItem>
+      <NavigationItem link="/orders">Orders</NavigationItem>
     </ul>
   );
 };
